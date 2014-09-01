dd4eff909e542166cb700621d7de2a05c9dbac64
vagrant <vagrant@precise32.(none)>
2014-09-01 09:35:54 +0000

    Added jQuery animation to landing page -cary

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+//  $(document).ready(function() {
+//	console.log("hello!");
+// });
+
+ //$(document).ready(function() {
+  	//$('.hero-content h3').click(function(){
+//	       console.log("hello!");
+//	});
+// });
+
+// $(document).ready(function() {
+//	$('.hero-content h3').click(function(){
+ //     subText = $(this).text();
+ //     $(this).text(subText + "!");	});
+// });
+
+
+// $(document).ready(function() {
+//	$('.hero-content h3').click(function(){
+//    subText = $(this).text();
+  //  $(this).text(subText + "!");	
+//      console.log('Click action triggered.');
+//});
+
+// $('.selling-points .point').hover(function() {
+//     console.log('Hover action triggered.');
+//     $(this).animate({'margin-top': '-10px'});
+//   });
+//});
+require("./landing");
+
