5fe20154d1b807d8c3d40cd61012861eb02c7d1c
vagrant <vagrant@precise32.(none)>
2014-10-06 09:51:07 +0000

    Made the sliders animate 'The Angular Way'.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -207,15 +207,15 @@ blocJams.service('SongPlayer', function($rootScope) {
 });
 
 blocJams.directive('slider', ['$document', function($document){
-  // Returns a number between 0 and 1 to determine where the mouse event happened along the slider bar.
-  var calculateSliderPercentFromMouseEvent = function($slider, event) {
-    var offsetX =  event.pageX - $slider.offset().left; // Distance from left
-    var sliderWidth = $slider.width(); // Width of slider
-    var offsetXPercent = (offsetX  / sliderWidth);
-    offsetXPercent = Math.max(0, offsetXPercent);
-    offsetXPercent = Math.min(1, offsetXPercent);
-    return offsetXPercent;
-  }
+   // Returns a number between 0 and 1 to determine where the mouse event happened along the slider bar.
+   var calculateSliderPercentFromMouseEvent = function($slider, event) {
+     var offsetX =  event.pageX - $slider.offset().left; // Distance from left
+     var sliderWidth = $slider.width(); // Width of slider
+     var offsetXPercent = (offsetX  / sliderWidth);
+     offsetXPercent = Math.max(0, offsetXPercent);
+     offsetXPercent = Math.min(1, offsetXPercent);
+     return offsetXPercent;
+   }
 
   var numberFromValue = function(value, defaultValue) {
     if (typeof value === 'number') {
@@ -239,11 +239,28 @@ blocJams.directive('slider', ['$document', function($document){
       onChange: '&'
     },
     link: function(scope, element, attributes) {
-      // These values represent the progress into the song/volume bar, and its max value.
-      // For now, we're supplying arbitrary initial and max values.
-      scope.value = 0;
-      scope.max = 100;
+       // These values represent the progress into the song/volume bar, and its max value.
+       // For now, we're supplying arbitrary initial and max values.
+       scope.value = 0;
+       scope.max = 200;
       var $seekBar = $(element);
+       var percentString = function () {
+         percent = Number(scope.value) / Number(scope.max)  * 100;
+         return percent + "%";
+       }
+ 
+       scope.fillStyle = function() {
+         return {width: percentString()};
+       }
+ 
+       scope.thumbStyle = function() {
+         return {left: percentString()};
+       }
+
+      scope.onClickSlider = function(event) {
+         var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
+         scope.value = percent * scope.max;
+       }
 
       attributes.$observe('value', function(newValue) {
         scope.value = numberFromValue(newValue, 0);
@@ -268,11 +285,11 @@ blocJams.directive('slider', ['$document', function($document){
         return {left: percentString()};
       }
 
-      scope.onClickSlider = function(event) {
-        var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
-        scope.value = percent * scope.max;
-        notifyCallback(scope.value);
-      }
+       scope.onClickSlider = function(event) {
+         var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
+         scope.value = percent * scope.max;
+       }
+
 
       scope.trackThumb = function() {
         $document.bind('mousemove.thumb', function(event){
@@ -283,20 +300,9 @@ blocJams.directive('slider', ['$document', function($document){
           });
         });
 
-        //cleanup
-        $document.bind('mouseup.thumb', function(){
-          $document.unbind('mousemove.thumb');
-          $document.unbind('mouseup.thumb');
-        }); 
-      };
-
-      var notifyCallback = function(newValue) {
-        if(typeof scope.onChange === 'function') {
-          scope.onChange({value: newValue});
-        }
-      };
     }
-  };
+ }
+ };
 }]);
 
 
